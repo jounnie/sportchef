b56ae041b8334bb424b55f4903e2cb802c96d239
Marcus Fihlon <marcus@fihlon.ch>
2015-03-13 11:13:39 +0100

    #16 Migration of Apache Commons Lang

--- server/src/main/java/ch/sportchef/server/representations/User.java
+++ server/src/main/java/ch/sportchef/server/representations/User.java
@@ -1,8 +1,8 @@
 package ch.sportchef.server.representations;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
