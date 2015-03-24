b6178f76e8759a61338d532cd2ebb03b07e181a1
Marcus Fihlon <marcus@fihlon.ch>
2015-03-24 22:51:44 +0100

    #25 Add test of run(…) method

--- src/test/java/ch/sportchef/server/AppShould.java
+++ src/test/java/ch/sportchef/server/AppShould.java
@@ -1,7 +1,13 @@
 package ch.sportchef.server;
 
+import ch.sportchef.server.healthchecks.UserServiceHealthCheck;
+import ch.sportchef.server.resources.UserResource;
+import ch.sportchef.server.services.UserService;
+import com.codahale.metrics.health.HealthCheckRegistry;
 import io.dropwizard.Bundle;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -10,6 +16,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -35,5 +42,22 @@ public class AppShould {
         new App().initialize(bootstrap);
 
         verify(bootstrap, times(1)).addBundle(any(Bundle.class));
+    }
+
+    @Test
+    public void runProperly() throws Exception {
+        final SportChefConfiguration configuration = mock(SportChefConfiguration.class);
+        final Environment environment = mock (Environment.class);
+        final HealthCheckRegistry healthCheckRegistry = mock(HealthCheckRegistry.class);
+        final JerseyEnvironment jerseyEnvironment = mock(JerseyEnvironment.class);
+
+        when(environment.healthChecks()).thenReturn(healthCheckRegistry);
+        when(environment.jersey()).thenReturn(jerseyEnvironment);
+
+        new App().run(configuration, environment);
+
+        assertThat(App.getService(UserService.class)).isNotNull();
+        verify(healthCheckRegistry, times(1)).register(eq("userService"), any(UserServiceHealthCheck.class));
+        verify(jerseyEnvironment, times(1)).register(any(UserResource.class));
     }
 }
