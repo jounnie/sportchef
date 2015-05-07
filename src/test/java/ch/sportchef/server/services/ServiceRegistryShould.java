5d9f852abd397a8e7e94494c364fecefc68ac74e
Marcus Fihlon <marcus@fihlon.ch>
2015-05-07 23:07:45 +0200

    #72 Coverage decreased due to missing contructor test

--- src/test/java/ch/sportchef/server/services/ServiceRegistryShould.java
+++ src/test/java/ch/sportchef/server/services/ServiceRegistryShould.java
@@ -4,11 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.management.ServiceNotFoundException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertTrue;
 
 public class ServiceRegistryShould {
 
     private static class TestService implements Service
     {
+    }
+
+    @Test
+    public void notBeInstantiable() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        final Constructor<ServiceRegistry> constructor = ServiceRegistry.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
     }
 
     @Test(expected = NullPointerException.class)
